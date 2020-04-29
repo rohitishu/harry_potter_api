d77619495a52b2055ecfb7c99404a6c597fcced0
Rohit Kumar <32030787+rohitishu@users.noreply.github.com>
2020-04-29 09:44:55 +0530

    added the search page
    user can search the character,staff and house of the harry potter movie

--- harry_potter/app.js
+++ harry_potter/app.js
@@ -3,9 +3,14 @@ var express=require("express");
 var app=express();
 var request=require("request");
 app.set("view engine","ejs");
+app.get("/",function(req, res) {
+    res.render("search");
+})
 app.get("/result",function(req,res)
 {
-  request("http://hp-api.herokuapp.com/api/characters",function(error,response,body)
+    var query=req.query.search;
+    var url="http://hp-api.herokuapp.com/api/"+query;
+  request(url,function(error,response,body)
    {
       if(!error && response.statusCode == 200)
       {
